2cbdc16344c6c2fa6c052068d9301815ebdfa984
litalz <litalbdbd@gmail.com>
2020-01-09 17:07:12 +0200

    start layout

---
+++ cv.js
@@ -0,0 +1,9 @@
+// function dataload() {
+//     let title = document.createElement("div");
+//     title.setAttribute("id", "title");
+//     title.innerHTML = "ABOUT ME";
+//     document.querySelector('#content').appendChild(title);
+// }
+
+
+// dataload();
